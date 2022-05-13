e03c11a36d4f4b8f5cbc4c69d8b62e0e4db1ceea
Arjun Singh <admin@arjunsingh.com.np>
2022-05-13 09:35:55 +0545

    Black Formatted

--- imageStegano/apps.py
+++ imageStegano/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ImagesteganoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'imageStegano'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "imageStegano"
