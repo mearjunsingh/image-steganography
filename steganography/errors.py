ddf8e313cf2aa503699f523aca4d3bf6946ec44f
Arjun Singh <admin@arjunsingh.com.np>
2022-08-09 08:26:33 +0545

    Worked on templates

--- steganography/errors.py
+++ steganography/errors.py
@@ -7,16 +7,32 @@ handler500 = "handler500"
 
 
 def handler400(request, exception=None):
-    pass
+    ctx = {
+        "error_code": 400,
+        "error_msg": "400 error",
+    }
+    return render(request, "error.html", ctx)
 
 
 def handler403(request, exception=None):
-    pass
+    ctx = {
+        "error_code": 400,
+        "error_msg": "400 error",
+    }
+    return render(request, "error.html", ctx)
 
 
 def handler404(request, exception=None):
-    pass
+    ctx = {
+        "error_code": 404,
+        "error_msg": "Page not found.",
+    }
+    return render(request, "error.html", ctx)
 
 
 def handler500(request, exception=None):
-    pass
+    ctx = {
+        "error_code": 500,
+        "error_msg": "Something happened. Could complete your action.",
+    }
+    return render(request, "error.html", ctx)
