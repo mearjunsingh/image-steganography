c9ddc52bcef3830bb06c511e122e1f63b1632106
Arjun Singh <admin@arjunsingh.com.np>
2022-11-25 00:54:55 +0545

    Clean Files

--- steganography/settings/base.py
+++ steganography/settings/base.py
@@ -74,13 +74,8 @@ WSGI_APPLICATION = "steganography.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("MYSQL_DATABASE_NAME"),
-        "USER": os.environ.get("MYSQL_USER"),
-        "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
-        "HOST": os.environ.get("MYSQL_HOST"),
-        "PORT": os.environ.get("MYSQL_PORT"),
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
--- steganography/settings/prod.py
+++ steganography/settings/prod.py
@@ -2,11 +2,11 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["example.com"]
+ALLOWED_HOSTS = ["demo.arjunsingh.com.np"]
 
-STATIC_URL = "static/"
-STATIC_ROOT = "static_CDN"
+STATIC_URL = "assets/"
+STATIC_ROOT = "/home/pynotesc/arjundemo/image-steganography/assets/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = "uploads"
+MEDIA_ROOT = "/home/pynotesc/arjundemo/image-steganography/media/"
